69e2ff923e714eaad20465e6d60ab376677dd034
Daneker Nurgaliyeva <d.nurgaliyeva@alabs.team>
2022-01-26 18:18:42 +0600

    changed node display name

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -99,8 +99,8 @@ pub mod opaque {
 }
 
 pub const VERSION: RuntimeVersion = RuntimeVersion {
-	spec_name: create_runtime_str!("node-frontier-template"),
-	impl_name: create_runtime_str!("node-frontier-template"),
+	spec_name: create_runtime_str!("ice-node"),
+	impl_name: create_runtime_str!("ice-node"),
 	authoring_version: 1,
 	spec_version: 1,
 	impl_version: 1,
