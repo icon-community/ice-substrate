ebadd5f3913e1cfa5a90726d3c869bb9d9558638
sudip ghimire <sudipghimire533@gmail.com>
2022-02-10 10:14:48 +0000

    Remove ice_address field from snapshotInfo. Add ServerResponse type

--- pallets/airdrop/src/types.rs
+++ pallets/airdrop/src/types.rs
@@ -4,8 +4,8 @@ use core::convert::Into;
 use frame_support::pallet_prelude::*;
 use frame_support::traits::Currency;
 use frame_system;
-use scale_info::prelude::*;
 use scale_info::TypeInfo;
+use serde::Deserialize;
 use sp_std::prelude::*;
 
 /// AccountId of anything that implements frame_system::Config
@@ -18,7 +18,8 @@ pub type BalanceOf<T> = <<T as Config>::Currency as Currency<AccountIdOf<T>>>::B
 pub type IconAddress = sp_std::vec::Vec<u8>;
 
 /// type that represnt the error that can occur while validation the signature
-#[cfg_attr(feature = "std", derive(Debug, Eq, PartialEq))]
+#[derive(Eq, PartialEq)]
+#[cfg_attr(feature = "std", derive(Debug))]
 #[cfg_attr(not(feature = "std"), derive(RuntimeDebug))]
 pub enum SignatureValidationError {
 	InvalidLength,
@@ -33,30 +34,38 @@ pub enum SignatureValidationError {
 #[derive(Encode, Decode, Clone, RuntimeDebug, TypeInfo)]
 #[scale_info(skip_type_params(T))]
 pub struct SnapshotInfo<T: Config> {
+	/// Icon address of this snapshot
 	pub icon_address: Vec<u8>,
-	pub ice_address: T::AccountId,
+
+	/// Total airdroppable-amount this icon_address hold
 	pub amount: BalanceOf<T>,
+
+	/// Indicator wather this icon_address holder is defi-user
 	pub defi_user: bool,
+
+	/// TODO: add description of this filed
 	pub vesting_percentage: u32,
+
+	/// indicator wather the user have claimmed the balance
+	// TODO:
+	// this field might need to be change when working with vesting
+	// might be change into airdropped_amount: BalanceOf<T>
 	pub claim_status: bool,
 }
 
 impl<T: Config> SnapshotInfo<T> {
+	/// Helper function to set icon_address in builder-pattern way
+	/// so that initilisation can be done in single line
 	pub fn icon_address(mut self, val: Vec<u8>) -> Self {
 		self.icon_address = val;
 		self
 	}
-
-	pub fn ice_address(mut self, val: <T as frame_system::Config>::AccountId) -> Self {
-		self.ice_address = val;
-		self
-	}
 }
 
+/// implement default values for snapshotInfo
 impl<T: Config> Default for SnapshotInfo<T> {
 	fn default() -> Self {
 		Self {
-			ice_address: <T as frame_system::Config>::AccountId::default(),
 			icon_address: sp_std::vec![],
 			amount: 0_u32.into(),
 			defi_user: false,
@@ -77,4 +86,25 @@ pub enum ClaimError {
 
 	/// some error while doing an http request
 	HttpError,
+}
+
+/// Structure expected to return from server when doing a request for details of icon_address
+#[derive(Deserialize, Encode, Decode, Clone, Default, Eq, PartialEq, TypeInfo, Copy)]
+#[cfg_attr(feature = "std", derive(Debug))]
+#[cfg_attr(not(feature = "std"), derive(RuntimeDebug))]
+pub struct ServerResponse {
+	// TODO: Add description of this field
+	omm: u128,
+
+	/// Amount to transfer in this claim
+	// TODO:
+	// is this amount to tranfer in this claim or tranfser in total?
+	#[serde(rename = "balanced")]
+	amount: u128,
+
+	// TODO: add description of this field
+	stake: u128,
+
+	/// Indicator weather this icon_address is defi_user or not
+	defi_user: bool,
 }
