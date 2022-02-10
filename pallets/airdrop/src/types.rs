d9a6032171af38bfeecc55ff164492d60d801afa
sudip ghimire <sudipghimire533@gmail.com>
2022-02-10 08:45:25 +0000

    Add snapshotinfo types. Remove SignedMessage type

--- pallets/airdrop/src/types.rs
+++ pallets/airdrop/src/types.rs
@@ -1,16 +1,24 @@
+use crate as airdrop;
+use airdrop::pallet::Config;
+use core::convert::Into;
+use frame_support::pallet_prelude::*;
+use frame_support::traits::Currency;
 use frame_system;
+use scale_info::prelude::*;
+use scale_info::TypeInfo;
+use sp_std::prelude::*;
 
 /// AccountId of anything that implements frame_system::Config
 pub type AccountIdOf<T> = <T as frame_system::Config>::AccountId;
 
+/// Type that represent the balance
+pub type BalanceOf<T> = <<T as Config>::Currency as Currency<AccountIdOf<T>>>::Balance;
+
 /// Type that represent IconAddress
-pub type IconAddress = sp_std::vec::Vec<u8>;
+pub type IconAddress = Vec<u8>;
 
-/// type that represnt the signed message
-pub type SignedMessage = sp_std::vec::Vec<u8>;
-
-#[cfg_attr(test, derive(Debug, Eq, PartialEq))]
 /// type that represnt the error that can occur while validation the signature
+#[cfg_attr(feature = "std", derive(Debug, Eq, PartialEq))]
 pub enum SignatureValidationError {
 	InvalidLength,
 	InvalidFormat,
@@ -19,4 +27,40 @@ pub enum SignatureValidationError {
 	MismatchedIceAddress,
 	Sha3Execution,
 	ECRecoverExecution,
+}
+
+#[derive(Encode, Decode, Clone, RuntimeDebug, TypeInfo)]
+#[scale_info(skip_type_params(T))]
+pub struct SnapshotInfo<T: Config> {
+	icon_address: Vec<u8>,
+	ice_address: T::AccountId,
+	amount: BalanceOf<T>,
+	defi_user: bool,
+	vesting_percentage: u32,
+	claim_status: bool,
+}
+
+impl<T: Config> SnapshotInfo<T> {
+	pub fn icon_address(mut self, val: Vec<u8>) -> Self {
+		self.icon_address = val;
+		self
+	}
+
+	pub fn ice_address(mut self, val: <T as frame_system::Config>::AccountId) -> Self {
+		self.ice_address = val;
+		self
+	}
+}
+
+impl<T: Config> Default for SnapshotInfo<T> {
+	fn default() -> Self {
+		Self {
+			ice_address: <T as frame_system::Config>::AccountId::default(),
+			icon_address: sp_std::vec![],
+			amount: 0_u32.into(),
+			defi_user: false,
+			vesting_percentage: 0,
+			claim_status: false,
+		}
+	}
 }
