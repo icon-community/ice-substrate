373395b1fcb68d29af4a28d7caba2f2772b4d862
0xrust <catalin@web3labs.com>
2022-07-04 19:27:25 +0300

    fix build

--- pallets/airdrop/src/tests/mock.rs
+++ pallets/airdrop/src/tests/mock.rs
@@ -43,7 +43,7 @@ frame_support::construct_runtime!(
 
 parameter_types! {
 	pub const BlockHashCount: u64 = 250;
-	pub const SS58Prefix: u8 = 42;
+	pub const SS58Prefix: u16 = 15253;
 }
 
 impl system::Config for Test {
