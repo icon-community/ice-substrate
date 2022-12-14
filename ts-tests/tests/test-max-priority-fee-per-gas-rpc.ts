bd949a72561cb34d10a385b2f14887caed72c220
vivek <viveksharmapoudel@gmail.com>
2022-12-14 22:02:05 +0545

    add evm test cases
    rebase

--- ts-tests/tests/test-max-priority-fee-per-gas-rpc.ts
+++ ts-tests/tests/test-max-priority-fee-per-gas-rpc.ts
@@ -17,13 +17,55 @@ describeWithIce("Ice RPC (Max Priority Fee Per Gas)", (context) => {
 
 	let nonce = 0;
 
+	async function createBlocks(block_count, priority_fees) {
+		const gasfee = context.ethersjs.getGasPrice();
+		for (var b = 0; b < block_count; b++) {
+			for (var p = 0; p < priority_fees.length; p++) {
+				await sendTransaction(context, {
+					from: GENESIS_ACCOUNT,
+					to: "0x0000000000000000000000000000000000000000",
+					data: "0x",
+					value: "0x00",
+					maxFeePerGas: gasfee,
+					maxPriorityFeePerGas: context.web3.utils.numberToHex(priority_fees[p]),
+					accessList: [],
+					nonce: nonce,
+					gasLimit: gasfee,
+					chainId: CHAIN_ID,
+				});
+				nonce++;
+			}
+			// await createAndFinalizeBlock(context.web3);
+		}
+	}
+
 	step("should default to zero on genesis", async function () {
 		let result = await customRequest(context.web3, "eth_maxPriorityFeePerGas", []);
 		expect(result.result).to.be.eq("0x0");
 	});
 
 	step("should default to zero on empty blocks", async function () {
+		// await createAndFinalizeBlock(context.web3);
 		let result = await customRequest(context.web3, "eth_maxPriorityFeePerGas", []);
 		expect(result.result).to.be.eq("0x0");
 	});
+
+	// If in the last 20 blocks at least one is empty (or only contains zero-tip txns), the
+	// suggested tip will be zero.
+	// That's the expected behaviour in this simplified oracle version: there is a decent chance of
+	// being able to include a zero-tip txn in a low congested network.
+	// step("maxPriorityFeePerGas should suggest zero if there are recent empty blocks", async function () {
+	// 	this.timeout(100000);
+
+	// 	for (let i = 0; i < 10; i++) {
+	// 		await createBlocks(1, [0, 1, 2, 3, 4, 5]);
+	// 	}
+	// 	// await createAndFinalizeBlock(context.web3);
+	// 	for (let i = 0; i < 9; i++) {
+	// 		await createBlocks(1, [0, 1, 2, 3, 4, 5]);
+	// 	}
+
+	// 	let result = (await customRequest(context.web3, "eth_maxPriorityFeePerGas", [])).result;
+	// 	expect(result).to.be.eq("0x0");
+	// });
 });
