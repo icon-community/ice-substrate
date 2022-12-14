eb72919cba4ee304c7d7abff77f61a9e9e3cbe7e
vivek <viveksharmapoudel@gmail.com>
2022-12-14 21:54:53 +0545

    added test contract for ts-test

---
+++ ts-tests/contracts/ECRecoverTests.sol
@@ -0,0 +1,10 @@
+pragma solidity >=0.8.0;
+
+
+contract ECRecoverTests {
+    function ecrecover(bytes memory input) public returns(bytes memory) {
+        address ecrecoverAddress = address(0x0000000000000000000000000000000000000001);
+        (bool success, bytes memory returnData) = ecrecoverAddress.call(input);
+        return returnData;
+    }
+}
