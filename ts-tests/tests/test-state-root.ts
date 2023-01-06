d14943e181c9ee89c649c4a4b03d9cef33edcec3
viveksharmapoudel <viveksharmapoudel@gmail.com>
2023-01-06 19:08:17 +0545

    Incorporate frontier ts tests into ice-substrate (#236)
    * ts-tests setup
    
    * added test contract for ts-test
    
    * add evm test cases
    
    * add ts-test github workflow
    
    * refractor test github action workflow
    
    * add genesis evm account in frost
    
    * ts-test test-nounce increase timeout time
    
    * refractor ts-tests
    
    * refractor test workflow
    
    Co-authored-by: ibrizsabin <101165234+ibrizsabin@users.noreply.github.com>
    
    fix conflict

--- ts-tests/tests/test-state-root.ts
+++ ts-tests/tests/test-state-root.ts
@@ -8,7 +8,6 @@ describeWithIce("Ice RPC (State root hash)", (context) => {
 	step("should calculate a valid intermediate state root hash", async function () {
 		block = await context.web3.eth.getBlock("latest");
 		expect(block.stateRoot.length).to.be.equal(66); // 0x prefixed
-		// expect(block.stateRoot).to.not.be.equal("0x1b09325951dfa631735e3455c08e0ba284d7e4940e3502e955b39616971882e8");
 	});
 
 	step("hash should be unique between blocks", async function () {
