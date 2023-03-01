f3394c945044cfa92a7df469d60c3bb5529a2fcc
abhi <abhijeet.bhagat@gmx.com>
2023-03-01 19:40:21 +0530

    bump spec versions and release versions (#284)

--- runtime/arctic/src/lib.rs
+++ runtime/arctic/src/lib.rs
@@ -197,7 +197,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("arctic-testnet"),
 	impl_name: create_runtime_str!("arctic-testnet"),
 	authoring_version: 1,
-	spec_version: 3,
+	spec_version: 4,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
